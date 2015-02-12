5831a88768948b0a29f25905dab0d310db0dede9
yojan shrestha <sickgrin@gmail.com>
2015-02-12 09:57:11 -0600

    Issue: style not being added in content_Script

--- vernaCs.js
+++ vernaCs.js
@@ -1,1 +1,11 @@
-console.log('yoloman');
+(function() {
+  var tooltipWrap = document.createElement('div');
+  var tooltip = "<div id='fuckme'> Yoloswag you know what I mean</div>";
+	
+	//Need id for parent id to have innerHTML
+  tooltipWrap.id = "vernaTip";
+  document.body.appendChild(tooltipWrap);
+
+  document.querySelector('#vernaTip').innerHTML = tooltip;
+	document.querySelector('#fuckme').className = "swaggerwanker";
+}());

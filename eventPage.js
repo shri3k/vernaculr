d193083789812186471d42eb4262c3214b808f78
yojan shrestha <sickgrin@gmail.com>
2015-02-12 00:01:14 -0600

    Init commit

---
+++ eventPage.js
@@ -0,0 +1,16 @@
+(function() {
+  var menuItem = {
+    "id": "vernaculMe",
+    "title": "Find meaning",
+    "contexts": ["selection"]
+  };
+
+  chrome.contextMenus.create(menuItem);
+  chrome.contextMenus.onClicked.addListener(function(clickData) {
+    console.dir(clickData);
+    if (clickData.menuItemId === "vernaculMe" && clickData.selectionText) {
+      console.log("You wanna search this: %s", clickData.selectionText);
+    }
+  });
+
+}());
